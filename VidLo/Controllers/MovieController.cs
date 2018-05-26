ff7b17aa6beb8089dc970e782af634bd646b24c8
Abobolais <e_sylvere@hotmail.com>
2018-05-26 13:20:43 -0400

    Add Movies controller

--- VidLo/Controllers/MovieController.cs
+++ VidLo/Controllers/MovieController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VidLo.Models;
 
 namespace VidLo.Controllers
 {
     public class MovieController : Controller
     {
         // GET: Movie
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private object GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Man on fire"},
+
+            };
         }
     }
 }
